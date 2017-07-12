0efed8feeb47677d773b71ba868a4cad4c5fb0d3
utest4all <bogdank@leantegra.com>
2017-07-12 13:26:26 +0300

    Final app

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-**Building**
-https://javatutorial.net/java-servlet-example
+**Install tomcat8**<br />
+sudo apt install tomcat8<br />
 
-**Running**
+**Building**<br />
+https://github.com/myCustomDemo/servlet.git<br />
+cd servlet<br />
+mvn clean package<br />
+sudo chmod 777 target/test-1.war<br />
+sudo cp target/test-1.war /var/lib/tomcat8/webapps<br />
+
+**Running**<br />
+sudo service tomcat8 restart
+
+**Testing**<br />
+curl -l "http://localhost:8080/test-1/hello"
 
--- src/main/java/SimpleServlet.java
+++ src/main/java/SimpleServlet.java
@@ -6,12 +6,14 @@ import javax.servlet.http.HttpServletResponse;
 
 public class SimpleServlet extends HttpServlet {
 
+
     private static final long serialVersionUID = -4751096228274971485L;
 
     @Override
     protected void doGet(HttpServletRequest reqest, HttpServletResponse response)
             throws ServletException, IOException {
-        response.getWriter().println("Hello World!");
+        response.setContentType("text/html");
+        response.getOutputStream().println("Hello World!");
     }
 
     @Override
